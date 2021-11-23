aa8865018883cf914f70f59e93386a8991d89467
AlumnoM_Sanchez <javitosanchz32@gmail.com>
2021-11-23 10:22:41 +0100

    Tercer commit con main

--- src/es/centroafuera/Main.java
+++ src/es/centroafuera/Main.java
@@ -3,6 +3,7 @@ package es.centroafuera;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Hola");
+        System.out.println("Que tal estas?");
     }
 }
